dbf80fec9d1426a60116aefa29646bb63611db39
Andrea Verlicchi <verlicchia@git.yoox.net>
2017-04-12 12:30:06 +0200

    Added moment.js

--- js/main.js
+++ js/main.js
@@ -11,13 +11,14 @@
             let output = document.getElementsByClassName('events__data')[0];
             if (output) {
                 output.innerHTML = data.map(event => {
+                    let relativeTime = moment(event.start.local).calendar();
                     let startDateTime = event.start.local.split('T');
                     let startDate = dateFormat(startDateTime[0]);
                     let startTime = timeFormat(startDateTime[1]);
                     let endTime = timeFormat(event.end.local.split('T')[1]);
                     return `<article class="event">
                         <h2 class="event__title"><a href="${event.url}">${event.name.text}</a></h2>
-                        <div class="event__time">${startDate} <time>${startTime}</time> &rarr; <time>${endTime}</time></div>
+                        <div class="event__date">${relativeTime} &dash; <span class="event__date__time">dalle ${startTime} alle ${endTime}</span></div>
                         <div class="event__image"><img src="${event.logo.url}" width="200" height="100"></div>
                         <div class="event__description">${event.description.html}</div>
                     </article>`;
