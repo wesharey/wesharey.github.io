eb89c0b46d43ad088d022667cd5ff0331cefed18
Andrea Verlicchi <verlicchia@git.yoox.net>
2017-04-13 19:05:19 +0200

    Added ajax calls to events availability

--- js/main.js
+++ js/main.js
@@ -1,34 +1,55 @@
 (function(){
-    const dateFormat = dateString => {
-        return dateString.split('-').reverse().join('/');
-    };
     const timeFormat = timeString => {
         return timeString.split(':').slice(0, 2).join(':');
     };
-    //fetch("./mocks/weshare-events.now.sh.v2.json")
+
+    const getEventsHtml = data => data.map(event => {
+        let relativeTime = moment(event.start.local).calendar();
+        let startDateTime = event.start.local.split('T');
+        let startTime = timeFormat(startDateTime[1]);
+        let endTime = timeFormat(event.end.local.split('T')[1]);
+        return `<article class="event">
+            <h2 class="event__title"><a href="${event.url}">${event.name.text}</a></h2>
+            <div class="event__date">${relativeTime} &dash; <span class="event__date__time">dalle ${startTime} alle ${endTime}</span></div>
+            <img class="event__image" data-original="${event.logo.url}" width="200" height="100">
+            <div class="event__description">${event.description.html}</div>
+            <div class="event__action" data-id="${event.id}" data-url="${event.url}" data-capacity="${event.capacity}"></div>
+        </article>`;
+    }).join('');
+
+    // Pre-check
+    let eventsDataEls = document.getElementsByClassName('events__data');
+    if (!eventsDataEls.length) return;
+    let eventsDataEl = eventsDataEls[0];
+
+    //fetch("./mocks/weshare-events.now.sh.v3.json")
     fetch("https://weshare-events.now.sh/")
         .then(data => data.json())
         .then(data => {
-            let output = document.getElementsByClassName('events__data')[0];
-            if (!output) { return; } 
-            output.innerHTML = data.map(event => {
-                let relativeTime = moment(event.start.local).calendar();
-                let startDateTime = event.start.local.split('T');
-                let startDate = dateFormat(startDateTime[0]);
-                let startTime = timeFormat(startDateTime[1]);
-                let endTime = timeFormat(event.end.local.split('T')[1]);
-                
-                return `<article class="event">
-                    <h2 class="event__title"><a href="${event.url}">${event.name.text}</a></h2>
-                    <div class="event__date">${relativeTime} &dash; <span class="event__date__time">dalle ${startTime} alle ${endTime}</span></div>
-                    <img class="event__image" data-original="${event.logo.url}" width="200" height="100">
-                    <div class="event__description">${event.description.html}</div>
-                    <a class="event__cta" href="${event.url}">
-                        <span class="event__cta__book-now">Prenota il tuo posto</span>
-                        <!--<span class="event__cta__available-seats">N posti disponibili</span>-->
-                    </a>
-                </article>`;
-            }).join('');
+            eventsDataEl.innerHTML = getEventsHtml(data);
+
+            let actionEls = eventsDataEl.querySelectorAll('.event__action');
+            actionEls.forEach(el => {
+                fetch("https://weshare-events.now.sh/?eventid="+el.dataset.id)
+                    .then(data => data.json())
+                    .then(data => {
+                        console.log(data);
+                        let seatsLeft = el.dataset.capacity - data.partecipants;
+                        if (seatsLeft) {
+                            el.innerHTML = `<a class="event__cta" href="${el.dataset.url}">
+                                <span class="event__cta__book-now">Prenota il tuo posto</span>
+                                <span class="event__cta__available-seats">${seatsLeft} posti disponibili</span>
+                            </a>`;
+                        }
+                        else {
+                            el.innerHTML = `<a class="event__sold-out" href="${el.dataset.url}">Tutto esaurito :(</a>`
+                        }
+                    });
+            });
+
+
+
+
             new LazyLoad({elements_selector: ".event__image"});
         });
 }());
