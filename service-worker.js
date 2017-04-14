8ef5096feeefd8c17e09fcdfc658f1b3a903ed1e
Andrea Verlicchi <verlicchia@git.yoox.net>
2017-04-14 17:29:50 +0200

    Serving content network-first, all other requests cache-first

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,24 @@
-const offlineCacheKey = "offline";
-const offlineURL = "offline.html";
 const assetsCacheKey = "assets";
+const appShellCacheKey = "shell";
+
+const localCacheFiles = [
+    /*"",
+    "/offline.html",*/
+    "/img/weshare.svg",
+    "/css/main.css",
+    "/js/main.js"
+];
+const remoteCacheFiles = [
+    "https://cdnjs.cloudflare.com/ajax/libs/fetch/2.0.3/fetch.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/locale/it.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/7.2.0/lazyload.transpiled.min.js"
+];
 
 const isNavigateRequest = request => {
     return request.mode === "navigate" ||
         (request.method === "GET" &&
         request.headers.get("Accept").includes("text/html"));
-};
-
-const isImageRequest = request => {
-    return request.headers.get("Accept").includes("image");
-};
-
-const isCSSRequest = request => {
-    return request.headers.get("Accept").includes("text/css");
-};
-
-const serveFakeCSS = request => {
-    return new Response("", {headers: {"Content-Type": "text/css"}});
 };
 
 const addToCache = (request, response) => {
@@ -41,22 +42,15 @@ const fetchFromCache = request => {
 
 self.addEventListener("install", event => {
     event.waitUntil(
-        fetch(new Request(offlineURL)).then(response => {
-            return caches.open(assetsCacheKey).then(cache => {
-                return cache.put(offlineURL, response);
-            });
-        })
+        caches.open(appShellCacheKey).then(cache => {
+            return cache.addAll(localCacheFiles);
+        }).then(() => self.skipWaiting())
     );
 });
 
-const offlinePage = () => {
-    return caches.open(offlineCacheKey).then(cache => {
-        return cache.match(offlineURL);
-    });
-};
-
 self.addEventListener("fetch", event => {
     let request = event.request;
+    let urlObj = new URL(request.url);
     if (isNavigateRequest(request)) {
         // Content - network first, then cache, then offline page
         console.log("Content request");
@@ -67,31 +61,15 @@ self.addEventListener("fetch", event => {
                 .catch(() => offlinePage())
         );
     }
-    else if (isImageRequest(request)) {
-        // Images - cache first, then network (and add to cache), then ...
-        console.log("Image request");
+    else if ((remoteCacheFiles.indexOf(request.url) !== -1) ||
+        (localCacheFiles.indexOf(urlObj.pathname) !== -1)) {
+        console.log("Respond with file for " + request.url);
         event.respondWith(
-            fetchFromCache(request) // Cache first
-                .catch(() => fetch(request) // But if not in cache, fetch
-                .then(response => addToCache(request, response))) // If fetch was successful, add to cache for later
-                .catch(() => console.log("Oops. Unable to respond to image request")) // If fetch failed... (lame solution - figure out what to do with it)
-        );
-    }
-    else if (isCSSRequest(request)) {
-        // CSS - network first, then cache, then fake CSS
-        event.respondWith(
-            fetch(request)
-                .then((response) => addToCache(request, response))
-                .catch(() => fetchFromCache(request))
-                .catch(() => serveFakeCSS())
+            fetchFromCache(request)
+                .catch(() => fetch(request))
         );
     }
     else {
-        console.log("Other request", {
-            accept: request.headers.get("Accept"),
-            mode: request.mode,
-            method: request.method,
-            requestObj: request
-        });
+        console.log("Failed to respond from cache", request.url);
     }
 });
