d01c8301a1d27b2a97c3691ab2cb25082a74ffb4
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2017-04-20 11:25:18 +0200

    Removed the self-executing function

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,34 @@
-(g => {
-    'use strict';
+'use strict';
 
-    // Load the sw-tookbox library.
-    importScripts('/js/sw-toolbox/sw-toolbox.js');
+// Load the sw-tookbox library.
+importScripts('/js/sw-toolbox/sw-toolbox.js');
 
-    // Network first get index.html
-    toolbox.router.get('/index.html', toolbox.networkFirst);
+// Turn on debug logging, visible in the Developer Tools' console.
+// toolbox.options.debug = true;
 
-    // Turn on debug logging, visible in the Developer Tools' console.
-    // g.toolbox.options.debug = true;
+// HTTP GET requests to eventbrite images are cached for 30 days, but max 10 images
+toolbox.router.get(/^https:\/\/img\.evbuc\.com\//, toolbox.cacheFirst, {
+    cache: {
+        name: 'eventbrite-images',
+        maxEntries: 10,
+        maxAgeSeconds: 2592000 //30 days * 24 * 60 * 60 seconds
+    }
+});
 
-    // Set up a handler for HTTP GET requests to eventbrite images
-    g.toolbox.router.get(/^https:\/\/img\.evbuc\.com\//, g.toolbox.cacheFirst, {
-        cache: {
-            name: 'eventbrite-images',
-            maxEntries: 10,
-            maxAgeSeconds: 2592000 //30 days * 24 * 60 * 60 seconds
-        }
-    });
+// HTTP GET requests to eventbrite data are cached 1 day and refreshed after every call
+toolbox.router.get(/^https:\/\/weshare\-events\.now\.sh\//, toolbox.fastest, {
+    cache: {
+        name: 'eventbrite-data',
+        maxAgeSeconds: 86400 //1 day * 24 * 60 * 60 seconds
+    }
+});
 
-    // Set up a handler for HTTP GET requests to eventbrite data
-    g.toolbox.router.get(/^https:\/\/weshare\-events\.now\.sh\//, g.toolbox.fastest, {
-        cache: {
-            name: 'eventbrite-data',
-            maxAgeSeconds: 86400 //1 day * 24 * 60 * 60 seconds
-        }
-    });
-    
-    // Set up a handler for HTTP GET requests to CDNJS
-    g.toolbox.router.get(/^https:\/\/cdnjs\.cloudflare\.com\//, g.toolbox.cacheFirst, {
-        cache: {
-            name: 'cdnjs-data',
-            maxEntries: 4
-        }
-    });
+// HTTP GET requests to CDNJS are cached like forever :)
+toolbox.router.get(/^https:\/\/cdnjs\.cloudflare\.com\//, toolbox.cacheFirst, {
+    cache: {
+        name: 'cdnjs-data'
+    }
+});
 
-    g.toolbox.router.default = g.toolbox.networkFirst;
-
-})(self);
+// All other requests are served network-first - so they're cached for offline
+toolbox.router.default = toolbox.networkFirst;
