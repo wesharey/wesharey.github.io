a77e41aaed4f6fc5ba2aec2f5fd1208c40b1060d
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2017-04-20 12:18:59 +0200

    Cache policy update

--- service-worker.js
+++ service-worker.js
@@ -6,27 +6,25 @@ importScripts('/js/sw-toolbox/sw-toolbox.js');
 // Turn on debug logging, visible in the Developer Tools' console.
 // toolbox.options.debug = true;
 
-// HTTP GET requests to eventbrite images are cached for 30 days, but max 10 images
+// HTTP GET requests to eventbrite images are cached forever, but max 12 images
 toolbox.router.get(/^https:\/\/img\.evbuc\.com\//, toolbox.cacheFirst, {
     cache: {
         name: 'eventbrite-images',
-        maxEntries: 10,
-        maxAgeSeconds: 2592000 //30 days * 24 * 60 * 60 seconds
+        maxEntries: 12
+    }
+});
+
+// HTTP GET requests to CDNJS are cached forever (version changes when file changes)
+toolbox.router.get(/^https:\/\/cdnjs\.cloudflare\.com\//, toolbox.cacheFirst, {
+    cache: {
+        name: 'cdnjs-data'
     }
 });
 
 // HTTP GET requests to eventbrite data are cached 1 day and refreshed after every call
-toolbox.router.get(/^https:\/\/weshare\-events\.now\.sh\/$/, toolbox.fastest, {
+toolbox.router.get("https://weshare-events.now.sh/", toolbox.fastest, {
     cache: {
         name: 'eventbrite-data',
-        maxAgeSeconds: 86400 //1 day * 24 * 60 * 60 seconds
-    }
-});
-
-// HTTP GET requests to CDNJS are cached like forever :)
-toolbox.router.get(/^https:\/\/cdnjs\.cloudflare\.com\//, toolbox.cacheFirst, {
-    cache: {
-        name: 'cdnjs-data'
     }
 });
 
