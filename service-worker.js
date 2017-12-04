381c678a905efc7f098d5df9d32b66bf5bf8db01
Andrea Verlicchi <verlicchia@yoox.net>
2017-12-04 12:38:54 +0100

    Quotes and stuff

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,31 @@
-'use strict';
+"use strict";
 
 // Load the sw-tookbox library.
-importScripts('/js/sw-toolbox/sw-toolbox.js');
+importScripts("/js/sw-toolbox/sw-toolbox.js");
 
 // Turn on debug logging, visible in the Developer Tools' console.
 // toolbox.options.debug = true;
 
 // HTTP GET requests to eventbrite images are cached forever, but max 12 images
 toolbox.router.get(/^https:\/\/img\.evbuc\.com\//, toolbox.cacheFirst, {
-    cache: {
-        name: 'eventbrite-images',
-        maxEntries: 12
-    }
+	cache: {
+		name: "eventbrite-images",
+		maxEntries: 12
+	}
 });
 
 // HTTP GET requests to CDNJS are cached forever (version changes when file changes)
 toolbox.router.get(/^https:\/\/cdnjs\.cloudflare\.com\//, toolbox.cacheFirst, {
-    cache: {
-        name: 'cdnjs-data'
-    }
+	cache: {
+		name: "cdnjs-data"
+	}
 });
 
 // HTTP GET requests to eventbrite data are cached 1 day and refreshed after every call
-toolbox.router.get("https://weshare-events.now.sh/", toolbox.fastest, {
-    cache: {
-        name: 'eventbrite-data',
-    }
+toolbox.router.get("https://www.eventbriteapi.com/", toolbox.fastest, {
+	cache: {
+		name: "eventbrite-data",
+	}
 });
 
 // All other requests are served network-first - so they're cached for offline
